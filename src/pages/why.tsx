40f2459c456e80bd543d2dca55b4bf64887fcb96
saufth <saufth@gmail.com>
2023-06-13 16:51:00 -0600

    Why page completed

--- src/pages/why.tsx
+++ src/pages/why.tsx
@@ -1,23 +1,149 @@
-// Styles
-import styles from '../styles/pages/WhyPage.module.css'
-
 /**
 * This page define the reason why of the organization
 * @returns The WhyPage component
 */
 const WhyPage = () => {
   return (
-    <div className={styles.div}>
+    <div className='px-5% pt-36 md:pt-44 pb-24 space-y-8'>
       <header>
         <h1>
-          Lorem ipsum
+          Problemática y razón de ser
         </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis asperiores
-          illo qui eum. Nemo dicta pariatur praesentium itaque, ut maxime nisi culpa numquam
-          officiis hic non assumenda quod ea eaque!
-        </p>
       </header>
+      <div className='space-y-6'>
+        <p>
+          Históricamente, el recurso humano en las empresas ha sido una problemática
+          de costos fijos y conflictos con sindicatos. En noviembre del 2022 más
+          de 150 mil empleados fueron despedidos en empresas de renombre como Amazon,
+          Meta y Twitter. Pero, detrás de la excesiva rotación de personal que experimentan
+          día a día las empresas, se esconden perdidas millonarias en dos sentidos:
+          el primero es en el proceso de reclutamiento y capacitación de los nuevos
+          elementos de la organización y segundo (el más grave), las pérdidas que
+          experimenta la empresa por NO tener a su personal alineado, medido,
+          capacitado e incentivado.
+        </p>
+        <p>
+          Aparentemente, una de las soluciones más obvias es la inminente totalización
+          de tecnologías y digitalización de los distintos procesos de la empresa. Este
+          proceso ayudará a que los procesos operativos sean automatizados y basados en
+          estadísticas y tecnologías sofisticadas. Por esta razón, el departamento de RH
+          debe asumir un rol relevante para liderar la agenda de modernización. Pero, por
+          otro lado, las iniciativas de transformación digital generalmente fracasan porque
+          no se tiene en cuenta que este es un proceso predominantemente “humano” y que su
+          éxito no tiene nada que ver con las tecnologías adecuadas, sino con las personas
+          adecuadas y, sobre todo, con las competencias adecuadas. Este aspecto es un factor
+          clave y diferenciador que garantiza el éxito en el proceso. Algunas competencias a
+          las que nos referimos son las siguientes: actitud proactiva, cultura de logro,
+          gestión del cambio, liderazgo, resiliencia, entre muchas otras. Teniendo éstas
+          mayor relevancia que, incluso, títulos académicos y años de experiencia en el puesto.
+        </p>
+      </div>
+      <article className='space-y-8'>
+        <header>
+          <h2>
+            ¿Qué distingue a KEYPERSPOT como empresa de RRHH?
+          </h2>
+        </header>
+        <div className='space-y-6'>
+          <p>
+            La razón de ser de Keyperspot, a diferencia de otras empresas de RR. HH,
+            es que va directamente ligada tanto a la estrategia global de la empresa,
+            como a los objetivos específicos de la misma. Es el vínculo que permite la
+            continuidad entre el factor humano y los demás elementos dentro del modelo
+            de negocio de una organización.
+          </p>
+          <p>
+            Por otro lado, en Keyperspot entendemos que las expectativas de los colaboradores
+            y equipos de trabajo son más altas que nunca. Ofrecerles una experiencia excepcional
+            es un gran reto, pues no es solamente escucharlos, sino entender quiénes son como
+            personas y qué es lo que realmente necesitan a niveles que solo las compañías que
+            buscan inspirar a sus equipos y clientes se dan el tiempo de profundizar.
+          </p>
+          <p>
+            Humanizar las tecnologías, conectar los valores humanos y las estadísticas
+            de resultados es a lo que nosotros denominamos “Potenciar el factor humano”.
+          </p>
+        </div>
+      </article>
+      <article className='space-y-8'>
+        <header>
+          <h2>
+            Muestro manifiesto
+          </h2>
+        </header>
+        <ul className='space-y-6'>
+          <li>
+            <p>
+              1-Sé auténtico; predica con el ejemplo.
+            </p>
+          </li>
+          <li>
+            <p>
+              2-Sé directo. Di la verdad, por dura que sea.
+            </p>
+          </li>
+          <li>
+            <p>
+              3-Escucha.
+            </p>
+          </li>
+          <li>
+            <p>
+              4-Sé receptivo.
+            </p>
+          </li>
+          <li>
+            <p>
+              5-Separa claramente las opiniones de los hechos.
+            </p>
+          </li>
+          <li>
+            <p>
+              6-Respeta la confidencialidad de la información.
+            </p>
+          </li>
+          <li>
+            <p>
+              7-Comprométete con los procesos.
+            </p>
+          </li>
+          <li>
+            <p>
+              8-Sé consciente de aquello que desconoces.
+            </p>
+          </li>
+          <li>
+            <p>
+              9-Guía, pero no controles.
+            </p>
+          </li>
+          <li>
+            <p>
+              10-Sé optimista.
+            </p>
+          </li>
+          <li>
+            <p>
+              11-Ofrece consejos factibles, no difusos.
+            </p>
+          </li>
+          <li>
+            <p>
+              12-Cuestiona y sé firme, pero nunca destructivo.
+            </p>
+          </li>
+          <li>
+            <p>
+              13-Sé empático.
+            </p>
+          </li>
+          <li>
+            <p>
+              14-No supongas.
+            </p>
+          </li>
+        </ul>
+      </article>
     </div>
   )
 }
